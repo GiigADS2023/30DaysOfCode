c6526a0f8fd4e348c6a5eee30da8e835e2b264a7
Giorgia Batista Schmidt <126842688+GiigADS2023@users.noreply.github.com>
2024-12-30 10:41:24 -0300

    Day 1 - Data Types

---
+++ Day 1 - Data Types/Day 1 - Data Types/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_1___Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0d;
+            string s = "HackerRank";
+
+            // Declare second integer, double, and String variables.
+            int i2;
+            double d2;
+            string s2;
+
+            // Read and save an integer, double, and String to your variables.
+            i2 = int.Parse(Console.ReadLine());
+            d2 = double.Parse(Console.ReadLine());
+            s2 = Console.ReadLine();
+
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine(String.Format("{0:0}", i + i2));
+
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(String.Format("{0:0.0}", d + d2));
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s + " " + s2);
+
+        }
+    }
+}
