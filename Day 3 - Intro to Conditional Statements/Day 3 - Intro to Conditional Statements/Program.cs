f0c16dcba97d61cbdf0a09b32ab8c84b07b85630
Giorgia Batista Schmidt <126842688+GiigADS2023@users.noreply.github.com>
2025-01-03 00:30:44 -0300

    Day 3 - Intro to Conditional Statements

---
+++ Day 3 - Intro to Conditional Statements/Day 3 - Intro to Conditional Statements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_3___Intro_to_Conditional_Statements
+{
+    internal class Program
+    {
+        static bool checkNumber(int myNumber)
+        {
+            bool isOdd_number = false;
+            int rem = myNumber % 2;
+            isOdd_number = rem != 0;
+            return isOdd_number;
+        }
+
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+            bool isOdd = checkNumber(N);
+
+            if (isOdd) Console.WriteLine("Weird");
+            else
+            {
+                if ((N >= 2) && (N <= 5))
+                {
+                    Console.WriteLine("Not Weird");
+                }
+                else
+                {
+                    if ((N >= 6) && (N <= 20))
+                    {
+                        Console.WriteLine("Weird");
+                    }
+                    else
+                    {
+                        if (N > 20) Console.WriteLine("Not Weird");
+                    }
+                }
+            }
+        }
+    }
+}
